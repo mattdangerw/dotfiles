3b87fa35f36d044221b20bb042d4f910290075db
Matt Watson <mattdangerw@gmail.com>
2022-05-30 21:25:01 -0700

    switch to conda

--- home/p10k.zsh
+++ home/p10k.zsh
@@ -34,7 +34,7 @@
     # os_icon               # os identifier
     dir                     # current directory
     vcs                     # git status
-    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    anaconda                # conda environment (https://conda.io/)
     # =========================[ Line #2 ]=========================
     newline                 # \n
     prompt_char             # prompt symbol
@@ -46,12 +46,12 @@
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
     status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
     asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-    anaconda                # conda environment (https://conda.io/)
     pyenv                   # python environment (https://github.com/pyenv/pyenv)
     goenv                   # go environment (https://github.com/syndbg/goenv)
     nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
